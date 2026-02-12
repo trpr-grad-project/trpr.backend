4fd71210d7bb4b4a44e2e1cdc9ea4fd6b293c955
OmarOsamaWT <omar.osama@tildetech.ae>
2026-02-12 21:10:29 +0200

    added paginate request to the controller

--- Modules/Notifications/src/External/Modules.Notifications.Presentation/Controllers/v1/TemplateController.cs
+++ Modules/Notifications/src/External/Modules.Notifications.Presentation/Controllers/v1/TemplateController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Application.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Modules.Notifications.Application.Dtos.Requests;
@@ -30,6 +31,15 @@ namespace Modules.Notifications.Presentation.Controllers.v1
         {
             var Id = await templateService.UpdateTemplate(id, dto, cancellationToken);
             return Ok(Id);
+        }
+
+        // TODO: Youssef 
+        // add this to the template service and implement the pagination logic 
+        // ps. the paginateion dto template has a create method that you can use;
+        [HttpGet]
+        public async Task<ActionResult<PaginationDto<Template>>> PaginateTemplates([FromQuery] int page = 1, [FromQuery] int pageSize = 20, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException("");
         }
     }
 }
