8ee6e9ffa5f845c2bbdc89bda510839701a67c42
Yussuf-Abokresha <yussufmohammad46@gmail.com>
2026-06-26 15:14:17 +0300

    add support request migration

---
+++ Modules/Users/src/External/Modules.Users.Infrastructure/EntityConfigs/SupportRequestConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Modules.Users.Domain.Entities;
+
+namespace Modules.Users.Infrastructure.EntityConfigs
+{
+    public class SupportRequestConfig : IEntityTypeConfiguration<SupportRequest>
+    {
+        public void Configure(EntityTypeBuilder<SupportRequest> builder)
+        {
+            builder.ToTable("SupportRequests");
+
+            builder.HasKey(sr => sr.Id);
+
+            builder.Property(sr => sr.Subject)
+                .IsRequired();
+
+            builder.Property(sr => sr.Description)
+                .IsRequired();
+
+
+            builder.Property(sr => sr.Status)
+                .IsRequired();
+                
+
+            builder.HasOne(sr => sr.user)
+                .WithMany()
+                .HasForeignKey(sr => sr.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(sr => sr.Subject); 
+            builder.HasIndex(sr => sr.Status);
+        }
+    }
+}
---
+++ Modules/Users/src/External/Modules.Users.Infrastructure/Migrations/20260626120937_AddSupportRequests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Users.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSupportRequests : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SupportRequests",
+                schema: "usr",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    subject = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_support_requests", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_support_requests_users_user_id",
+                        column: x => x.user_id,
+                        principalSchema: "usr",
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_support_requests_status",
+                schema: "usr",
+                table: "SupportRequests",
+                column: "status");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_support_requests_subject",
+                schema: "usr",
+                table: "SupportRequests",
+                column: "subject");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_support_requests_user_id",
+                schema: "usr",
+                table: "SupportRequests",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupportRequests",
+                schema: "usr");
+        }
+    }
+}
